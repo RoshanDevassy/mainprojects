3cf4692cbc40a37b947e4157e4bf9525b6c50e1d
Roshan-Devassy <roshandevassy101112@gmail.com>
2025-02-20 17:54:21 +0530

    checking commit

--- budget_calculator/src/components/navbar.jsx
+++ budget_calculator/src/components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="h-fit min-w-min flex flex-grow justify-center bg-slate-400">
         <nav className="bg-slate-400 p-0 xsm:p-1 3xl:p-4 flex flex-grow justify-center flex-wrap">
           <ul className="flex flex-col xsm:flex-row xsm:gap-5 items-center gap-0 text-black font-semibold flex-wrap justify-center 2xl:pb-1">
-            <li className="hover:cursor-pointer">
+            <li className="hover:cursor-pointer active:border-b-2">
               <Link
                 to="/home"
                 className="font-bold text-xs xsm:text-sm sm:text-base md:text-xl xl:text-2xl 2xl:text-[26px] 3xl:text-3xl"
