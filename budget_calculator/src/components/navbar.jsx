a8ac09ad69436846f690e4a4fb0c09ee07451424
Roshan-Devassy <roshandevassy101112@gmail.com>
2025-02-20 17:59:29 +0530

    checking commit

--- budget_calculator/src/components/navbar.jsx
+++ budget_calculator/src/components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="h-fit min-w-min flex flex-grow justify-center bg-slate-400">
         <nav className="bg-slate-400 p-0 xsm:p-1 3xl:p-4 flex flex-grow justify-center flex-wrap">
           <ul className="flex flex-col xsm:flex-row xsm:gap-5 items-center gap-0 text-black font-semibold flex-wrap justify-center 2xl:pb-1">
-            <li className="hover:cursor-pointer active:border-b-2">
+            <li className="hover:cursor-pointer hover:bg-slate-600 rounded-md px-2 py-1 hover:text-white">
               <Link
                 to="/home"
                 className="font-bold text-xs xsm:text-sm sm:text-base md:text-xl xl:text-2xl 2xl:text-[26px] 3xl:text-3xl"
@@ -14,7 +14,7 @@ const Navbar = () => {
                 Home
               </Link>
             </li>
-            <li className="hover:cursor-pointer">
+            <li className="hover:cursor-pointer hover:bg-slate-600 rounded-md px-2 py-1 hover:text-white">
               <Link
                 to="/about"
                 className="font-bold px-1 2xl:px-[6px] text-xs xsm:text-sm sm:text-base md:text-xl xl:text-2xl 2xl:text-[26px] 3xl:text-3xl"
@@ -22,7 +22,7 @@ const Navbar = () => {
                 About
               </Link>
             </li>
-            <li className="hover:cursor-pointer">
+            <li className="hover:cursor-pointer hover:bg-slate-600 rounded-md px-2 py-1 hover:text-white">
               <Link
                 to="/app"
                 className="font-bold text-xs xsm:text-sm sm:text-base md:text-xl xl:text-2xl 2xl:text-[26px] 3xl:text-3xl"
